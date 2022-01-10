c85052cee17933b4867dda4260e4e426cc469d70
dheiejsbd <sulwoo01@gmail.com>
2022-01-10 12:23:40 +0900

    hlod pakage

---
+++ hlod/com.unity.hlod/Editor/Batcher/IBatcher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Unity.HLODSystem.Utils;
+using UnityEngine;
+
+namespace Unity.HLODSystem
+{
+    public interface IBatcher : IDisposable
+    {
+        
+        void Batch(Vector3 rootPosition, DisposableList<HLODBuildInfo> targets, Action<float> onProgress);
+
+    }
+}
