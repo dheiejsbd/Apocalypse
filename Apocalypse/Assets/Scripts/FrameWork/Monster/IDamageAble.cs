7f5db2365d11d639cc936122d333a15a25bead78
dheiejsbd <sulwoo01@gmail.com>
2022-01-10 12:20:32 +0900

    DamageSyster

---
+++ Apocalypse/Assets/Scripts/FrameWork/Monster/IDamageAble.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace FrameWork.Monster
+{
+    public interface IDamageAble
+    {
+        void TakeHit(int Damage);
+    }
+}
---
+++ Apocalypse/Assets/Scripts/Monster/MonsterController.IDamageAble.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+using FrameWork.Monster;
+
+namespace Apocalypse
+{
+    public partial class MonsterController : IDamageAble
+    {
+        public void TakeHit(int Damage)
+        {
+            HP -= Damage;
+        }
+    }
+}
